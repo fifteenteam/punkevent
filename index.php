8c60c72c125dbbc72bdf388862462e785720a551
fifteenteam <molchanovdesign@yandex.ru>
2013-10-10 05:05:00 +0400

    /

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <body onload="AjaxLoadRequest('main-list','http://spbphoto.net/startbl/load-list.php')">
 	<?php
 // GLOBALS
-	$logined=anon;
+	$logined=anonimusvasya;
 	?>
 	<div class="navbar navbar-inverse">
 		<div class="navbar-inner">
