0464d8eb5816cb8dae29dd045a1ac199bc0f3000
fifteenteam <molchanovdesign@yandex.ru>
2013-10-10 20:12:02 +0400

    del

--- currentevent.php
+++ currentevent.php
@@ -183,21 +183,7 @@
 									<p class="">Присоединяйся!</p>
 								</div>
 								<div class="ktogde">
-									<p>	
-										<a href="#" class="username">Vasya</a>
-										<span>&nbsp&nbsp&nbspСейчас&nbsp&nbsp&nbsp</span>
-										<a class=""href="#">tut</a>
-									</P>
-									<p>
-										<a href="#" class="username">Vasya</a>
-										<span>&nbsp&nbsp&nbspСейчас&nbsp&nbsp&nbsp</span>
-										<a class=""href="#">tut</a>
-									</P>
-									<p>
-										<a href="#" class="username">Vasya</a>
-										<span>&nbsp&nbsp&nbspСейчас&nbsp&nbsp&nbsp</span>
-										<a class=""href="#">tut</a>
-									</P>       	
+									     	
 								</div>
 							</div>
 							<div class="block secondwin">
